16018793cd954c8879132e994f8986bcc291d740
gasper! <36280998+gaspercode@users.noreply.github.com>
2024-02-07 13:08:31 -0500

    New queries

---
+++ MIN & MAX.sql
@@ -0,0 +1,16 @@
+
+#This query finds the Pokemon with the lowest Attack stat.
+
+SELECT name, MIN(Attack) AS Weakest
+FROM pokemon
+GROUP BY Name
+ORDER BY Weakest LIMIT 10;
+
+
+#This query finds the Pokemon with the highest Speed stat, while filtering out special Pokemon forms.
+
+SELECT name, MAX(Speed) AS Fastest
+FROM pokemon
+WHERE Form IS NULL
+GROUP BY Name
+ORDER BY Fastest DESC LIMIT 10;
