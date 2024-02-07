77da2caf3862d91a6f5f47acf804b24e41d5a8fd
gasper! <36280998+gaspercode@users.noreply.github.com>
2024-02-07 11:58:09 -0500

    Update Creating a Database.sql

--- Creating a Database.sql
+++ Creating a Database.sql
@@ -16,21 +16,22 @@ CREATE TABLE  food (
 #Here we insert values into our menu.
 INSERT INTO food (food_name, food_type, contains_meat, price) 
     VALUES
-    ('Pork Bun', 'Appetizer', 1, 4.50),
+	('Pork Bun', 'Appetizer', 1, 4.50),
 	('Spring Roll', 'Appetizer', 1, 3.00),
-    ('Egg Roll', 'Appetizer', 1, 3.50),
-    ('Xiao Long Bao', 'Appetizer', 1, 5.00),
-    ('Siu Mai', 'Appetizer', 1, 4.00),
-    ('Crab Rangoons', 'Appetizer', 1, 5.00),
-    ('Kung Pao Chicken', 'Main', 1, 10.00),
-    ('Szechuan Beef', 'Main', 1, 12.00),
-    ('Mapo Tofu', 'Main', 0, 9.00),
-    ('Barbecue Pork', 'Main', 1, 11.00),
-    ('Chow Fun', 'Main', 1, 9.00),
-    ('Shrimp Fried Rice', 'Main', 1, 9.00),
-    ('Sesame Balls', 'Dessert', 0, 7.00),
-    ('Chinese Donuts', 'Dessert', 0, 7.00),
-    ('Fortune Cookie', 'Dessert', 0, 0.00);
+	('Egg Roll', 'Appetizer', 1, 3.50),
+	('Xiao Long Bao', 'Appetizer', 1, 5.00),
+	('Siu Mai', 'Appetizer', 1, 4.00),
+	('Crab Rangoons', 'Appetizer', 1, 5.00),
+	('Kung Pao Chicken', 'Main', 1, 10.00),
+	('Szechuan Beef', 'Main', 1, 12.00),
+	('Mapo Tofu', 'Main', 0, 9.00),
+	('Barbecue Pork', 'Main', 1, 11.00),
+	('Chow Fun', 'Main', 1, 9.00),
+	('Shrimp Fried Rice', 'Main', 1, 9.00),
+	('Sesame Balls', 'Dessert', 0, 7.00),
+	('Chinese Donuts', 'Dessert', 0, 7.00),
+	('Fortune Cookie', 'Dessert', 0, 0.00);
+    
     
 #If we were to delete this table, this is how:
 DROP TABLE food;
