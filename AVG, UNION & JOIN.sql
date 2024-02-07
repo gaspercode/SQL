a18af7adaeaeab10b54474ced0682044f157c379
gasper! <36280998+gaspercode@users.noreply.github.com>
2024-02-07 13:15:06 -0500

    Update AVG, UNION & JOIN.sql

--- AVG, UNION & JOIN.sql
+++ AVG, UNION & JOIN.sql
@@ -2,36 +2,31 @@
 #Pokemon can be up to 2 types and there are 2 columns for this (Type1 and Type2).
 #Specified that if a Type is Null, it is not counted.
 
-SELECT 
-    Type, COUNT(*) AS TotalCount
-FROM
-    (SELECT 
-        Type1 AS Type
-    FROM
-        pokemon UNION ALL SELECT 
-        Type2 AS Type
-    FROM
-        pokemon
-    WHERE
-        Type2 IS NOT NULL) AS Types
-GROUP BY Type
-ORDER BY TotalCount DESC;
+SELECT type,
+       Count(*) AS TotalCount
+FROM   (SELECT type1 AS Type
+        FROM   pokemon
+        UNION ALL
+        SELECT type2 AS Type
+        FROM   pokemon
+        WHERE  type2 IS NOT NULL) AS Types
+GROUP  BY type
+ORDER  BY totalcount DESC;
 
-
-SELECT 
-    Type, AVG(pokemon.Total) AS AverageTotal
-FROM
-    (SELECT 
-        Type1 AS Type, Total
-    FROM
-        pokemon UNION ALL SELECT 
-        Type2 AS Type, Total
-    FROM
-        pokemon
-    WHERE
-        Type2 IS NOT NULL) AS Types
-        JOIN
-    pokemon ON Types.Type = pokemon.Type1
-        OR Types.Type = pokemon.Type2
-GROUP BY Type
-ORDER BY AverageTotal DESC;
+#Using the above, we can also do things like find the average stats of each type:
+    
+SELECT type,
+       Avg(pokemon.total) AS AverageTotal
+FROM   (SELECT type1 AS Type,
+               total
+        FROM   pokemon
+        UNION ALL
+        SELECT type2 AS Type,
+               total
+        FROM   pokemon
+        WHERE  type2 IS NOT NULL) AS Types
+       JOIN pokemon
+         ON Types.type = pokemon.type1
+             OR Types.type = pokemon.type2
+GROUP  BY type
+ORDER  BY averagetotal DESC; 
