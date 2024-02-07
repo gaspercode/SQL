60aec09bea56766d5d2e13fe2d1415e096f7a2ef
gasper! <36280998+gaspercode@users.noreply.github.com>
2024-02-07 13:13:17 -0500

    Update MIN & MAX.sql

--- MIN & MAX.sql
+++ MIN & MAX.sql
@@ -1,17 +1,24 @@
 #Working with a dataset of Pokemon.
 
 #This query finds the Pokemon with the lowest Attack stat.
-
-SELECT name, MIN(Attack) AS Weakest
-FROM pokemon
+  
+SELECT 
+    name, MIN(Attack) AS Weakest
+FROM
+    pokemon
 GROUP BY Name
-ORDER BY Weakest LIMIT 10;
+ORDER BY Weakest
+LIMIT 10;
 
 
 #This query finds the Pokemon with the highest Speed stat, while filtering out special Pokemon forms.
 
-SELECT name, MAX(Speed) AS Fastest
-FROM pokemon
-WHERE Form IS NULL
+SELECT 
+    name, MAX(Speed) AS Fastest
+FROM
+    pokemon
+WHERE
+    Form IS NULL
 GROUP BY Name
-ORDER BY Fastest DESC LIMIT 10;
+ORDER BY Fastest DESC
+LIMIT 10;
